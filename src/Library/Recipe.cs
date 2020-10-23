e67b4a589202fc53376bedf9ca87b08ecd67e0c1
ilima12091 <limaignacio12091@gmail.com>
2020-10-22 21:11:49 -0300

    Agrego los archivos del ejercicio

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -33,6 +33,17 @@ namespace Full_GRASP_And_SOLID.Library
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
+            Console.WriteLine($"Costo total de producción: '{this.GetProductionCost().ToString()}'");
+        }
+
+        public double GetProductionCost()
+        {
+            double result = 0;
+            foreach(Step step in this.steps)
+            {
+                result += step.StepCost();
+            }
+            return result;
         }
     }
 }
